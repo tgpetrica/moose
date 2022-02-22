0565775f00f6291152500930b08a2daf468bad6f
Teodor <teodor.petrica@rovacc.ro>
2022-02-23 00:25:18 +0200

    updated

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Moose Project'
+copyright = '2022, tgpetrica'
+author = '@tgpetrica'
 
 release = '0.1'
-version = '0.1.0'
+version = '0.1.1'
 
 # -- General configuration
 
